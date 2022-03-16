3016bb9b4e8b5ee0f22a4e970eddb4a81bf82cd1
Douglas A B Novato <douglasabnovato@gmail.com>
2022-03-16 10:28:52 -0300

    lógica para exibir um filme por vez

--- main.js
+++ main.js
@@ -5,18 +5,6 @@ const config = {
     language: 'language=pt-BR'
 }
 
-/**
-    Chave da API (v3 auth)
-    0e3950318bf412e11272f2f58c14e062
-
-    Exemplo de Requisição de API
-    https://api.themoviedb.org/3/movie/550?api_key=0e3950318bf412e11272f2f58c14e062
-
-    Token de Leitura da API (v4 auth)
-    eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZTM5NTAzMThiZjQxMmUxMTI3MmYyZjU4YzE0ZTA2MiIsInN1YiI6IjYyMDI3MTI4ZTI0ZTNhMDAyMDQxZGY2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f9GBTuzeyehxbSk5QJluMqpr69F3HFsZmgQRXmzMMdI
-
-*/
-
 function getApi(url_themoviesdborg){
     let request = new XMLHttpRequest()
 
@@ -39,20 +27,21 @@ function createMov(movie){
         const div_description = document.createElement('div')
         div_description.innerText = movie.overview
 
-
-    
     div_cards.appendChild(div_poster)
     div_cards.appendChild(div_title)
     div_cards.appendChild(div_description)
-
-    console.log(movie)
 
     return div_cards
 
 }
 
 function main(){
-    const page = 1
+
+    const maxPage = 500
+    const maxMovie = 19
+
+    const page = Math.floor(Math.random() * maxPage)
+    const itemMovie = Math.floor(Math.random() * maxMovie) 
     const BASE_URL = config.api_base_url
     const API_KEY = config.api_key
 
@@ -60,11 +49,10 @@ function main(){
     let box = JSON.parse(data)
     let movies = box.results
     let div_box_movies = document.getElementById('movies')
+ 
+    let mov = createMov(movies[itemMovie])
+    div_box_movies.appendChild(mov)
     
-    movies.forEach(movie => {
-        let mov = createMov(movie)
-        div_box_movies.appendChild(mov)
-    })
 }
 
 main()
